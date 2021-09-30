0703a24347dd8c00a1301dbc371a59a3417667fd
sebastien-david <sebastien.david08@gmail.com>
2021-09-30 11:24:53 +0200

    Ajoutez des fichiers projet.

---
+++ Quete_1_POO-Les_Constructeus/Building.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Building
+{
+
+	private int _etageBatiment;
+	private double _hauteurBatiment;
+
+	public Building (int etage) : this (etage, etage*3)
+	{ }
+
+	public Building(int etage, double hauteur)
+	{
+		_etageBatiment = etage;
+		_hauteurBatiment = hauteur;
+	}
+
+	public double GetFloorMaxSize ()
+    {
+		return _hauteurBatiment / _etageBatiment;
+    }
+
+	public int GetFloorCount()
+    {
+		return _etageBatiment;
+    }
+
+	public double GetSize()
+	{
+		return _hauteurBatiment;
+	}
+
+}
---
+++ Quete_1_POO-Les_Constructeus/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quete_1_POO_Les_Constructeus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building batiment1 = new Building(20, 55);
+            Building batiment2 = new Building(15);
+
+            Console.WriteLine(" Concernant le batiment 1 (nombre d'etage et hauteur)\n");
+            Console.WriteLine(" Le nombre d'étage est de: " + (batiment1.GetFloorCount()));
+            Console.WriteLine(" La hauteur du batiment est de: " + (batiment1.GetSize()) + " m");
+            Console.WriteLine(" La hauteur maximale d'un étage est de: " + (batiment1.GetFloorMaxSize()) + " m");
+
+            Console.WriteLine("\n Concernant le batiment 2 (nombre d'etage)\n");
+            Console.WriteLine(" Le nombre d'étage est de: " + (batiment2.GetFloorCount()));
+            Console.WriteLine(" La hauteur du batiment est de: " + (batiment2.GetSize()) + " m");
+            Console.WriteLine(" La hauteur maximale d'un étage est de: " + (batiment2.GetFloorMaxSize()) + " m");
+
+            Console.ReadLine();
+        }
+    }
+}
